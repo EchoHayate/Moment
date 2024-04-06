7e6bc225ac68d46b6e7fc941e6b1517a3d3694ac
Allen <sw2016@hw.ac.uk>
2024-04-06 14:39:50 +0100

    latest version

--- moment/Config/UIConfig.swift
+++ moment/Config/UIConfig.swift
@@ -15,9 +15,9 @@ import UIKit
 func rgbColor(r: Int,g:Int,b:Int) -> UIColor{
     if #available(iOS 10.0, *){
         return UIColor(displayP3Red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: 1)
-    return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: 1)
-        
+     
     }
+    return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: 1)
 }
 
 
